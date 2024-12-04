ce3f30cf5bcbfe9957f6abb9a7d350b80af754f5
Ron <rrosas@unal.edu.co>
2024-12-03 20:21:29 -0500

    actualización codigo principal con subida de las tres tablas a SQL

--- SQL Queries/Query_1.sql
+++ SQL Queries/Query_1.sql
@@ -1,4 +1,12 @@
 CREATE DATABASE sentimental_analysis_db;
 SHOW DATABASES;
+
 -- Imprimir tabla con reviews
-SELECT * FROM mercadolibre_reviews;
+SELECT * FROM webscrapping_sources;
+
+LOAD DATA INFILE 'D:/Documents/Scrapping Sentimientos Project/Project_repository/Webscrapping/Fuentes_de_datos.csv'
+INTO TABLE webscrapping_sources
+FIELDS TERMINATED BY ',' 
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS; -- Ignora la fila de encabezados
+
